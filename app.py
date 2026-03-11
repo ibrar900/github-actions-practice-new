e17617b68f6437583e36a714556200f39d2f58fd
Ibrar Mahammad <ibrarahmad900@gmail.com>
2026-03-12 02:19:38 +0530

    changes updated in app file

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/health')
+@app.route("/health")
 def health():
-    return 'Server is up and running'
+    return "Server is up and running"
 
 
-app.run(debug=False, port=80)
+app.run(debug=False, host="0.0.0.0", port=80)
